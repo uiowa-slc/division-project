4368117bf97eafcdb0c88433368d9cb19d31394a
Dustin Quam <quamsta@gmail.com>
2013-10-21 10:14:11 -0500

    adapting some code

--- _config.php
+++ _config.php
@@ -1,31 +1,9 @@
-<?php
-
-global $project;
-$project = 'mysite';
-
-global $databaseConfig;
-$databaseConfig = array(
-	"type" => 'MySQLDatabase',
-	"server" => 'localhost',
-	"username" => 'root',
-	"password" => 'omega',
-	"database" => 'vp',
-	"path" => '',
-);
-
-MySQLDatabase::set_connection_charset('utf8');
-
-// Set the current theme. More themes can be downloaded from
-// http://www.silverstripe.org/themes/
-SSViewer::set_theme('simple');
-
-// Set the site locale
-i18n::set_locale('en_US');
-FulltextSearchable::enable();
-// Enable nested URLs for this site (e.g. page/sub-page/)
-if (class_exists('SiteTree')) SiteTree::enable_nested_urls();
-
+<?php 
 Director::set_environment_type("dev");
+
+
+SiteTree::add_extension('DivisionSiteTree');
+ContentController::add_extension('DivisionSiteTree_Controller');
 
 // add a button to remove formatting
 HtmlEditorConfig::get('cms')->insertButtonsBefore(
--- code/DivisionSiteTree_Controller.php
+++ code/DivisionSiteTree_Controller.php
@@ -1,78 +1,5 @@
 <?php
-class Page extends SiteTree {
-
-	private static $db = array(
-		
-	);
-
-	private static $has_one = array(
-		"BackgroundImage" => "Image",
-	);
-
-
-	private static $many_many = array (
-		"SidebarItems" => "SidebarItem"
-	);
-
-    private static $many_many_extraFields=array(
-        'SidebarItems'=>array(
-            'SortOrder'=>'Int'
-        )
-    );
-
-
-
-    private static $plural_name = "Pages";
-
-	private static $defaults = array (
-
-
-		"Content" =>
-			"<h1>H1. This is a very large header.</h1>
-<p>The first paragraph directly after an H1 is the lede paragraph and is styled with a larger font size than other paragraphs.</p>
-<h2>H2. This is a large header.</h2>
-<p>Nullam id dolor id nibh ultricies vehicula ut id elit. Cum sociis natoque penatibus et magnis dis parturient.</p>
-<h3>H3. This is a medium header.</h3>
-<p>Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh ut fermentum massa justo.</p>
-<h4>H4. This is a moderate header.</h4>
-<p>Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl.</p>
-<h5>H5. This is small header.</h5>
-<p>Cum sociis natoque penatibus magnis parturient montes, nascetur ridiculus mus. Sed consectetur est.</p>
-<h6>H6. This is very small header.</h6>
-<p>Donec id elit non mi porta gravida at eget metus. Curabitur blandit tempus porttitor.</p>"
-
-	);
-
-
-	public function getCMSFields(){
-		$f = parent::getCMSFields();
-		$f->addFieldToTab("Root.Main", new UploadField("BackgroundImage", "Background Image"), "Content");
-		
-
-		$gridFieldConfig = GridFieldConfig_RelationEditor::create();
-		
-		$row = "SortOrder";
-		$gridFieldConfig->addComponent($sort = new GridFieldSortableRows(stripslashes($row))); 
-		//$gridFieldConfig->addComponent(new GridFieldManyRelationHandler(), 'GridFieldPaginator');
-		$sort->table = 'Page_SidebarItems'; 
-		$sort->parentField = 'PageID'; 
-		$sort->componentField = 'SidebarItemID'; 
-
-		$gridField = new GridField("SidebarItems", "Sidebar Items", $this->SidebarItems(), $gridFieldConfig);
-		$f->addFieldToTab("Root.Sidebar", new LabelField("SidebarLabel", "<h2>Add sidebar items below</h2>"));
-		$f->addFieldToTab("Root.Sidebar", new LiteralField("SidebarManageLabel", '<p><a href="admin/sidebar-items" target="_blank">View and Manage Sidebar Items &raquo;</a></p>'));
-		$f->addFieldToTab("Root.Sidebar", $gridField); // add the grid field to a tab in the CMS
-		//$f->addFieldToTab("Root.Widgets", new WidgetAreaEditor("MyWidgetArea"));
-		return $f;
-	}
-
-
-    public function SidebarItems() {
-        return $this->getManyManyComponents('SidebarItems')->sort('SortOrder');
-    }
-	
-}
-class Page_Controller extends ContentController {
+class DivisionSiteTree_Controller extends Extension {
 
 	/**
 	 * An array of actions that can be accessed via a request. Each array element should be an action name, and the
@@ -92,8 +19,7 @@ class Page_Controller extends ContentController {
 	private static $allowed_actions = array (
 	);
 
-	public function init() {
-		parent::init();
+	public function onAfterInit() {
 
 		// Note: you should use SS template require tags inside your templates
 		// instead of putting Requirements calls here.  However these are
@@ -103,7 +29,6 @@ class Page_Controller extends ContentController {
 		Requirements::themedCSS('typography');
 		Requirements::themedCSS('form');
 
-    Requirements::block('division-bar/css/_division-bar.css');
 	}
 
 
