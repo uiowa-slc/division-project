3d3fcf0586f8058f99f09b8849971c7bb65ffff2
Dustin Quam <quamsta@gmail.com>
2013-10-21 14:34:12 -0500

    debugging

--- code/DivisionPage_Controller.php
+++ code/DivisionPage_Controller.php
@@ -21,6 +21,8 @@ class DivisionPage_Controller extends Extension {
 
 	public function onAfterInit() {
 
+		echo "hello";
+
 		// Note: you should use SS template require tags inside your templates
 		// instead of putting Requirements calls here.  However these are
 		// included so that our older themes still work
