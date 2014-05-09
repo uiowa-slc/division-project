96ecc6fcc7256169041f7c89baa463cf3d8034f5
Dustin Quam <quamsta@gmail.com>
2014-05-09 13:18:14 -0500

    added email address to siteconfig, reordered site config options

--- code/SiteConfigExtension.php
+++ code/SiteConfigExtension.php
@@ -8,6 +8,7 @@ class SiteConfigExtension extends DataExtension {
 	'PhoneNumber' =>'Text',
 	'FacebookLink' =>'Text',
 	'GroupSummary'=>'Text',
+	'EmailAddress' => 'Text'
     
 	);
 
@@ -16,12 +17,16 @@ class SiteConfigExtension extends DataExtension {
   );
   
   public function updateCMSFields(FieldList $fields){
-	  
-	  $fields->addFieldToTab('Root.Main', new TextField('TwitterLink', 'Twitter Account URL'));
-	  $fields->addFieldToTab('Root.Main', new TextField('FacebookLink', 'Facebook Account URL'));
+
+	  $fields->addFieldToTab('Root.Main', new TextareaField('GroupSummary', 'Group Summary'));
+
 	  $fields->addFieldToTab('Root.Main', new TextareaField('Address', 'Organization Address'));
 	  $fields->addFieldToTab('Root.Main', new TextareaField('PhoneNumber', 'Phone Number(s)'));
-	  $fields->addFieldToTab('Root.Main', new TextareaField('GroupSummary', 'Group Summary'));
+	  $fields->addFieldToTab('Root.Main', new TextareaField('EmailAddress', 'Email Address'));
+
+	  $fields->addFieldToTab('Root.Main', new TextField('TwitterLink', 'Twitter Account URL'));
+	  $fields->addFieldToTab('Root.Main', new TextField('FacebookLink', 'Facebook Account URL'));
+
 	  
 	  return $fields;
   }
--- templates/Includes/Footer.ss
+++ templates/Includes/Footer.ss
@@ -8,10 +8,14 @@
 				<% else %>
 					<p>The Division of Student Life fosters student success by creating and promoting inclusive educationally purposeful services and activities within and beyond the classroom.</p>
 				<% end_if %>
-				<p>$SiteConfig.Address<br>
+				<p>$SiteConfig.Address
 				<% if $SiteConfig.PhoneNumber %>
-				Phone: $SiteConfig.PhoneNumber
-				<% end_if %></p>
+					<br />Phone: $SiteConfig.PhoneNumber
+				<% end_if %>
+				<% if $SiteConfig.EmailAddress %>
+					<br />Email: <a href="mailto:{$EmailAddress}">$SiteConfig.EmailAddress</a>
+				<% end_if %>
+			</p>
 				
 			</div>
 			<div class="col-1-4 hide-print">
