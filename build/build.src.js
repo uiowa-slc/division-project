cff6515a02884d0e9c5f22b5ff16b65b66a02904
Dustin Quam <dustinquam@me.com>
2015-03-25 09:10:42 -0500

    fixed a few alignment issues

--- build/build.src.js
+++ build/build.src.js
@@ -11567,39 +11567,44 @@ $(document).ready(function() {
     });
 
 });
+$(function() {
 
-if ($('.flexslider').length){
+	if ($('.flexslider').length){
 
-	$('.flexslider').show();
+		$('.flexslider').show();
 
-	 //Flexslider
-	$('.flexslider').flexslider({
-		animation: "slide",
-		animationLoop: true,
-		itemMargin: 0,
-		minItems: 1,
-		maxItems: 1,
-		itemWidth: 500,
+		 //Flexslider
+		$('.flexslider').flexslider({
+			animation: "slide",
+			animationLoop: true,
+			itemMargin: 0,
+			minItems: 1,
+			maxItems: 1,
+			itemWidth: 500,
+		});
+	}
+
+	$('.nav-title a').click(function() {
+		$('.nav-main-wrapper').toggleClass('expand');
+		return false;
 	});
-}
 
-$('.nav-title a').click(function() {
-	$('.nav-main-wrapper').toggleClass('expand');
-	return false;
-});
+	/* FitVids */
+	$(".module .media").fitVids();
+	$(".hero-content").fitVids();
 
-/* FitVids */
-$(".module .media").fitVids();
-$(".hero-content").fitVids();
+	var bLazy = new Blazy({
+		selector: 'img,.lazy',
+	    breakpoints: [{
+	        width: 420, //max-width
+	        src: 'data-src-small'
+	    }, {
+	        width: 768, // max-width
+	        src: 'data-src-medium'
+	    }
+	   ]
+	});
 
-var bLazy = new Blazy({
-	selector: 'img,.lazy',
-    breakpoints: [{
-        width: 420, //max-width
-        src: 'data-src-small'
-    }, {
-        width: 768, // max-width
-        src: 'data-src-medium'
-    }
-   ]
+	$("body").removeClass("loading");
+	
 });
