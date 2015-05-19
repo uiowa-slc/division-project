45e4bd7b7edc4b3b2b0ced502e2e7e3449f819b6
Dustin Quam <dustinquam@me.com>
2015-05-19 13:51:22 -0500

    added shortcode link to content field

--- code/DivisionPage.php
+++ code/DivisionPage.php
@@ -45,6 +45,8 @@ class DivisionPage extends DataExtension {
 		}
 		$gridFieldConfig = GridFieldConfig_RelationEditor::create();
 
+		$f->addFieldToTab('Root.Main', new LiteralField('ShortcodeDocLink', '<a href="https://github.com/StudentLifeMarketingAndDesign/division-project/blob/2.0.x/docs/Shortcodes.md" target="_blank">How to use shortcodes (Flickr, Staff Spotlight, etc) &rarr;</a>'), 'Content');
+
 		$row = "SortOrder";
 		$gridFieldConfig->addComponent($sort = new GridFieldSortableRows(stripslashes($row)));
 
