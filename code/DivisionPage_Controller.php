812cf7e49fb41e9c1f0e74ee7214ee674951e730
shaanamin13 <shaan-amin@uiowa.edu>
2015-06-09 13:43:39 -0500

    Ability for adding buttons via url shortcode

--- code/DivisionPage_Controller.php
+++ code/DivisionPage_Controller.php
@@ -98,4 +98,27 @@ class DivisionPage_Controller extends Extension {
 		}
 	}
 
+	public static function ButtonHandler($arguments,$caption= null,$parser = null){
+		
+		if (empty($arguments['url'])) {
+		    return;
+		}
+		 
+		$customise = array();
+		/*** SET DEFAULTS ***/
+		$customise['Link'] = $arguments['url'];
+		$customise['Caption'] = $caption ? Convert::raw2xml($caption) : false;
+		 
+		//overide the defaults with the arguments supplied
+		$customise = array_merge($customise,$arguments);
+		 
+		//get our YouTube template
+		$template = new SSViewer('Button');
+		 
+		//return the customised template
+		return $template->process(new ArrayData($customise));		
+		
+	}
+
+
 }
---
+++ templates/Includes/Button.ss
@@ -0,0 +1,1 @@
+<a href="$Link" target="_blank" class="btn">$Caption</a>
