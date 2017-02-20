a43b95855bc33d9009bfdf9686fb94d0d5a55100
Brandon Ewoldt <bewoldt1@gmail.com>
2017-02-20 14:11:12 -0600

    adding upcoming events block

---
+++ code/blocks/UpcomingEventsBlock.php
@@ -0,0 +1,19 @@
+<?php
+
+class UpcomingEventsBlock extends Block{
+
+	private static $db = array(
+
+	);
+
+	private static $has_one = array(
+
+	);
+
+	function getCMSFields() {
+		$fields = parent::getCMSFields();
+
+		return $fields;
+	}
+
+}
