1a6732683ff6a306fc33bab277d781ea0bd476be
Dustin Quam <quamsta@gmail.com>
2017-06-29 10:34:04 -0500

    removed typekit warning since it's in the config file now

--- code/extensions/DivisionPage.php
+++ code/extensions/DivisionPage.php
@@ -60,10 +60,6 @@ class DivisionPage extends DataExtension {
 			$f->addFieldToTab("Root.Main", new LiteralField("AnalyticsWarning",
 				"<p class=\"message warning\">Google Analytics ID hasn't been set for this site. <a href=\"admin/settings/\"><em>You can set it in the site's settings &rarr;</em></a></p>"), "Title");
 		}
-		if(!$config->TypeKitID) {
-			$f->addFieldToTab("Root.Main", new LiteralField("TypeKitWarning",
-				"<p class=\"message warning\">Typekit ID hasn't been set for this site. <a href=\"admin/settings/#Root_TypeKit\"><em>You can set it in the site's settings &rarr;</em></a></p>"), "Title");
-		}
 
 		if ($metadataField = $f->fieldByName('Root.Main.Metadata')) {
 			$f->removeFieldFromTab('Root.Main', 'Metadata');
