df5b56c42c4b54872011f143d81a88bddcfab941
Jessica Brown <jessica-e-brown@uiowa.edu>
2017-07-26 09:06:47 -0500

    eventlistlimited function for sidebar load

--- code/blocks/UpcomingEventsBlock.php
+++ code/blocks/UpcomingEventsBlock.php
@@ -3,13 +3,19 @@
 class UpcomingEventsBlock extends Block{
 
 	private static $db = array(
-
+		'LimitEvents' => 'Int'
 	);
 
 	private static $has_one = array(
 
 	);
+	function EventListLimited(){
+		$calendar = LocalistCalendar::get()->First();
 
+		$eventList = $calendar->EventList(7);
+
+		return $eventList;
+	}
 	function getCMSFields() {
 		$fields = parent::getCMSFields();
 
