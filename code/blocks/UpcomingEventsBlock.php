c2d5930ae4e22267c355f6512d05584e1063d081
Dustin Quam <quamsta@gmail.com>
2017-08-08 09:20:57 -0500

    eventlist function

--- code/blocks/UpcomingEventsBlock.php
+++ code/blocks/UpcomingEventsBlock.php
@@ -3,7 +3,8 @@
 class UpcomingEventsBlock extends Block{
 
 	private static $db = array(
-		'LimitEvents' => 'Int'
+		'LimitEvents' => 'Int',
+		'EventTag' => 'Text'
 	);
 
 	private static $has_one = array(
@@ -22,6 +23,33 @@ class UpcomingEventsBlock extends Block{
 		$fields->renameField('Title', 'Title (default:Upcoming Events)');
 
 		return $fields;
+	}
+
+	function EventList() {
+
+		if (isset($this->EventTag)) {
+			$calendar = LocalistCalendar::get()->First();
+			$term = $this->EventTag;
+
+			$termFiltered = urlencode($term);
+
+			$events = $calendar->EventList(
+				$days = '200',
+				$startDate = null,
+				$endDate = null,
+				$venue = null,
+				$keyword = null,
+				$type = $term,
+				$distinct = 'true',
+				$enableFilter = false,
+				$searchTerm = null
+			);
+			return $events;
+
+		} else {
+			return null;
+		}
+
 	}
 
 }
