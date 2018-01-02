1f2c5956cf0344e6ab2774a96174da90cd8cdb23
Jessica Brown <jessica-e-brown@uiowa.edu>
2018-01-02 11:15:20 -0600

    namespacing blocks module

--- code/blocks/EmbedBlock.php
+++ code/blocks/EmbedBlock.php
@@ -6,6 +6,7 @@ use SilverStripe\CMS\Controllers\CMSPageEditController;
 use SilverStripe\Forms\DropdownField;
 use SilverStripe\Forms\TextField;
 use SilverStripe\Forms\CheckboxField;
+use MD\DviisionProject\Blocks\Model;
 
 class EmbedBlock extends Block{
     
