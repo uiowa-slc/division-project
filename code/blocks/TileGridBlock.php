5f18913969e078513b951455f830c5b2b46a11a7
Dustin Quam <quamsta@gmail.com>
2018-05-01 13:07:11 -0500

    tilegridblock fixes

--- code/blocks/TileGridBlock.php
+++ code/blocks/TileGridBlock.php
@@ -2,6 +2,9 @@
 
 class TileGridBlock extends Block{
 
+	private static $db = array(
+		'Source' => 'Varchar(155)'
+	);
 	private static $has_one = array(
 		'Holder' => 'SiteTree',
 	);
@@ -15,8 +18,25 @@ class TileGridBlock extends Block{
 	public function getCMSFields() {
 		$f = parent::getCMSFields();
 		$f->renameField('Title', 'Block title');
-		$f->addFieldToTab('Root.Main', new TreeDropdownField('HolderID', "Show the following page's children:", "SiteTree"));
-		$f->addFieldToTab('Root.Main', new TreeMultiselectField('CustomPages', 'Or Show The Following Pages:', "Page"));  
+
+		$f->removeByName('CustomPages');
+
+		$f->addFieldsToTab('Root.Main', array(
+			OptionsetField::create('Source', 'Show tiles from:',array(
+                'children' => 'A selected page\'s children',
+                'manual' => 'Manually selected pages',
+            )),
+
+			$treeDropdown = TreeDropdownField::create('HolderID', "Selected page's children:", "SiteTree"),
+			$customPagesSelect = TreeMultiselectField::create('CustomPages', 'Manually selected pages', 'SiteTree')
+		));
+
+		
+		// $treeDropdown->displayIf('Source')->isEqualTo('children');
+		// $customPagesSelect->displayIf('Source')->isEqualTo('manual');
+
+		$f->addFieldToTab('Root.Main', $treeDropdown);
+		$f->addFieldToTab('Root.Main', $customPagesSelect); 
 
 		return $f;
 
--- src/templates/Blocks/TileGridBlock.ss
+++ src/templates/Blocks/TileGridBlock.ss
@@ -2,7 +2,7 @@
 	<% if $ShowTitle %><h2 class="content-block-header content-block-header--with-padding header--centered header--small">$Title</h2><% end_if %>
 	<div class="tile-grid row small-up-1 medium-up-2 large-up-3 xlarge-up-4">
 
-	<% if $Holder %>
+	<% if $Source == 'children' %>
 	<% with $Holder %>
 		<% loop $Children %>
 			<a href="$Link" class="tile column dp-lazy" data-original="$BackgroundImage.FocusFill(300,300).URL">
@@ -10,7 +10,7 @@
 			</a>
 		<% end_loop %>
 	<% end_with %>
-	<% else_if $CustomPages %>
+	<% else_if $Source == 'manual' %>
 		<% loop $CustomPages %>
 			<a href="$Link" class="tile column dp-lazy" data-original="$BackgroundImage.FocusFill(300,300).URL">
 				<div class="tile__text"><h2 class="tile__header">$Title</h2></div>
