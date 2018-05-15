d89da1917f89bbe9f8366f9020a881593a8bea1e
Dustin Quam <quamsta@gmail.com>
2018-05-15 16:17:12 -0500

    plan page type

---
+++ code/model/planpage/PlanCategory.php
@@ -0,0 +1,66 @@
+<?php
+
+class PlanCategory extends DataObject {
+
+	private static $db = array(
+		'Title' => 'Varchar(155)',
+		'Content' => 'HTMLText',
+		'Column1Heading' => 'Varchar(155)',
+		'Column1Content' => 'HTMLText',
+		'Column2Heading' => 'Varchar(155)',
+		'Column2Content' => 'HTMLText'
+	);
+
+	private static $has_one = array(
+		'PlanPage' => 'PlanPage'
+	);
+
+	private static $has_many = array(
+		'PlanItems' => 'PlanItem'
+	);
+
+	private static $summary_fields = array(
+		'Title',
+		'Column1Heading',
+		'Column2Heading'
+	);
+
+	private static $defaults = array(
+		'Column1Heading' => 'Recommendation',
+		'Column2Heading' => 'Updates',
+		'Column2Content' => '<ul><li>No updates at this time.</li></ul>'
+	);
+
+	public function getCMSFields(){
+		$fields = FieldList::create();
+
+		$fields->push(TextField::create('Title', 'Title'));
+
+		$fields->push(HTMLEditorField::create('Content', 'Intro')->setRows(4));
+
+		// $fields->push(new TextField('Column1Heading'));
+
+		// $fields->push($column1Content = new HTMLEditorField('Column1Content'));
+		// $column1Content->setRows(4);
+
+		// $fields->push(new TextField('Column2Heading'));
+		// $fields->push($column2Content = new HTMLEditorField('Column2Content'));	
+		// $column2Content->setRows(4);
+
+		$config = GridFieldConfig_RelationEditor::create();
+		$planItemsField = new GridField('PlanItems', 'Plan items', $this->PlanItems());
+		$planItemsField->setConfig($config);
+		
+		$fields->push($planItemsField);
+
+		return $fields;
+
+	}
+
+	protected function onBeforeWrite(){
+
+
+		parent::onBeforeWrite();
+	}
+
+}
