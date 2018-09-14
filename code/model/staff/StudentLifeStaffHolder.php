231d3c2d21330722cdd2da84d8cefd536760e9df
Jessica Brown <jessica-e-brown@uiowa.edu>
2018-09-14 09:17:22 -0500

    division student life staff page types

---
+++ code/model/staff/StudentLifeStaffHolder.php
@@ -0,0 +1,118 @@
+<?php
+class StudentLifeStaffHolder extends Page {
+
+	private static $db = array(
+		'DepartmentID' => 'Int'
+	);
+
+	private static $has_one = array(
+
+	);
+
+	private static $has_many = array(
+	);
+
+	private static $feed_base = 'http://localhost:8888/student-life-at-iowa/staff';
+	// private static $feed_base = 'https://hulk.imu.uiowa.edu/student-life-at-iowa/news';
+
+	public function getCMSFields() {
+
+		$f = parent::getCMSFields();
+
+		$deptDropdownField = new StaffDeptDropdownField('DepartmentID', 'Department');
+
+		$f->addFieldToTab('Root.Main', $deptDropdownField, 'Content');
+	
+		return $f;
+	}
+
+	public static function getOrCreate(){
+		$holder = StudentLifeStaffHolder::get()->First();
+
+		if($holder){
+			return $holder;
+		}else{
+			$newHolder = new StudentLifeStaffHolder();
+			return $newHolder;
+		}
+	}
+
+	
+
+	public function getStaffTeamsFromFeed($filterType = null, $filterItem = null, $limit = null, $perPage = 10, $start = 0){
+
+		$feedBase = Config::inst()->get('StudentLifeStaffHolder', 'feed_base');
+		$deptId = $this->DepartmentID;
+		
+		$feedURL = $feedBase.'/departmentStaffFeed/'.$deptId;
+
+		if($start != 0){
+			$feedURL .='?start='.$start;
+		}
+		//print_r($feedURL);
+		$rawPostFeed = file_get_contents($feedURL);
+		$postsArray = json_decode($rawPostFeed, TRUE);
+
+		if(!isset($postsArray['posts'])){
+			return;
+		}
+
+	}
+
+}
+class StudentLifeStaffHolder_Controller extends Page_Controller {
+
+	/**
+	 * An array of actions that can be accessed via a request. Each array element should be an action name, and the
+	 * permissions or conditions required to allow the user to access it.
+	 *
+	 * <code>
+	 * array (
+	 *     'action', // anyone can access this action
+	 *     'action' => true, // same as above
+	 *     'action' => 'ADMIN', // you must have ADMIN permissions to access this action
+	 *     'action' => '->checkAction' // you can only access this action if $this->checkAction() returns true
+	 * );
+	 * </code>
+	 *
+	 * @var array
+	 */
+	private static $allowed_actions = array(
+		'go'
+	);
+
+	private static $url_handlers = array(
+		'$Action//$ID' => 'go'
+	);
+
+	public function init() {
+		parent::init();
+	}
+
+	public function go($request){
+
+		$action = $this->getRequest()->param('Action');
+		$id = $this->getRequest()->param('ID');
+
+		$posts = new ArrayList();
+
+		$getVars = $this->getRequest()->getVars();
+
+		if(isset($getVars['start'])){
+
+			$start = intval($getVars['start']);
+		}else{
+			$start = 0;
+		}
+		
+		$posts = $this->getStaffTeamsFromFeed(null, null, null, 10, $start);
+
+		$data = new ArrayData(array(
+			'FilterType' => $action,
+			'StaffTeams' => $posts,
+		));
+
+		return $this->customise($data)->renderWith(array('StudentLifeStaffHolder', 'Page'));
+	}
+
+}
