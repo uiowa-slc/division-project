b7dd4f8689b7188520e55bb112bdc99a91fe465d
Dustin Quam <quamsta@gmail.com>
2020-10-07 15:05:48 -0500

    updates

--- code/extensions/DivisionPage.php
+++ code/extensions/DivisionPage.php
@@ -1,30 +1,25 @@
 <?php
 
+use DNADesign\Elemental\Models\ElementalArea;
+use SilverStripe\AssetAdmin\Forms\UploadField;
 use SilverStripe\Assets\Image;
 use SilverStripe\Blog\Model\BlogPost;
-use SilverStripe\Core\ClassInfo;
-use SilverStripe\Forms\FieldList;
-use SilverStripe\SiteConfig\SiteConfig;
-use SilverStripe\Forms\DropdownField;
-use SilverStripe\Security\Permission;
-use SilverStripe\AssetAdmin\Forms\UploadField;
-use SilverStripe\Forms\LiteralField;
-use SilverStripe\Forms\TextField;
-use SilverStripe\Forms\TextareaField;
-use SilverStripe\Forms\GridField\GridFieldConfig_RelationEditor;
-use SilverStripe\Forms\GridField\GridField;
-use SilverStripe\Forms\LabelField;
-use SilverStripe\Forms\CheckboxField;
-use SilverStripe\Forms\HTMLEditor\HTMLEditorField;
-use SilverStripe\ORM\DataExtension;
-use MD\DivisionProject\DivisionPageController;
-use DNADesign\Elemental\Models\ElementalArea;
-use DNADesign\Elemental\Models\BaseElement;
-use SilverStripe\View\SSViewer;
-use SilverStripe\View\ArrayData;
 use SilverStripe\CMS\Model\SiteTree;
+use SilverStripe\Core\ClassInfo;
+use SilverStripe\Forms\CheckboxField;
+use SilverStripe\Forms\DropdownField;
+use SilverStripe\Forms\FieldList;
+use SilverStripe\Forms\GridField\GridFieldConfig_RelationEditor;
+use SilverStripe\Forms\HTMLEditor\HTMLEditorField;
+use SilverStripe\Forms\LiteralField;
+use SilverStripe\Forms\TextareaField;
+use SilverStripe\Forms\TextField;
+use SilverStripe\ORM\DataExtension;
+use SilverStripe\Security\Permission;
+use SilverStripe\SiteConfig\SiteConfig;
+use SilverStripe\View\ArrayData;
 use SilverStripe\View\Parsers\ShortcodeParser;
-
+use SilverStripe\View\SSViewer;
 
 class DivisionPage extends DataExtension {
 	private static $db = array(
@@ -36,7 +31,7 @@ class DivisionPage extends DataExtension {
 		'ShowChildPages' => 'Boolean(1)',
 		'ShowChildrenInDropdown' => 'Boolean(1)',
 		'FullImageAltText' => 'Text',
-		'DarkMode' => 'Boolean'
+		'DarkMode' => 'Boolean',
 	);
 
 	private static $has_one = array(
@@ -47,32 +42,32 @@ class DivisionPage extends DataExtension {
 		'AfterContent' => ElementalArea::class,
 		'BackgroundImage' => Image::class,
 		'FeatureHolderImage' => Image::class,
-		'OgImage' => Image::class
+		'OgImage' => Image::class,
 	);
-    private static $owns = array(
-        'BackgroundImage',
-        'OgImage',
-        'FeatureHolderImage',
+	private static $owns = array(
+		'BackgroundImage',
+		'OgImage',
+		'FeatureHolderImage',
 		'BackgroundImage',
 		'FeatureHolderImage',
 		'SidebarArea',
 		'AfterContentConstrained',
 		'BeforeContent',
 		'BeforeContentConstrained',
-		'AfterContent'
-    );
+		'AfterContent',
+	);
 	private static $many_many = array(
 
 	);
 
 	private static $belongs_many_many = array(
 		'TileGridBlocks' => 'TileGridBlock',
-		'PageSliderBlocks' => 'PageSliderBlock'
+		'PageSliderBlocks' => 'PageSliderBlock',
 	);
 
 	private static $many_many_extraFields = array(
 		'SidebarItems' => array(
-			'SortOrder'   => 'Int',
+			'SortOrder' => 'Int',
 		),
 	);
 
@@ -80,25 +75,24 @@ class DivisionPage extends DataExtension {
 		'BackgroundVideo' => 'Background Video, Overlay',
 		'BackgroundImage' => 'Background Image, Overlay',
 		'FullImage' => 'Full-sized image, not a background',
-		'NoSideNav' => 'No side navigation (even if this page has child pages)'
+		'NoSideNav' => 'No side navigation (even if this page has child pages)',
 	);
 
 	private static $plural_name = 'Pages';
 
 	private static $defaults = array(
 
-
 	);
 
-    private static $casting = [
-        'ExpandShortcode' => 'HTMLText'
-    ];
+	private static $casting = [
+		'ExpandShortcode' => 'HTMLText',
+	];
 
-	private static $hide_from_hierarchy = array(BlogPost::class,'Topic');
+	private static $hide_from_hierarchy = array(BlogPost::class, 'Topic');
 
-	public function ClassAncestry(){
+	public function ClassAncestry() {
 		$ancestryArray = ClassInfo::ancestry($this->owner->ClassName);
-		$ancestryString = implode(' ',$ancestryArray);
+		$ancestryString = implode(' ', $ancestryArray);
 
 		return $ancestryString;
 	}
@@ -109,7 +103,7 @@ class DivisionPage extends DataExtension {
 
 		$sidebarAreaField = $f->dataFieldByName('SidebarArea');
 
-		if($sidebarAreaField){
+		if ($sidebarAreaField) {
 			$sidebarAreaField->setTitle('Sidebar');
 			$f->removeByName('SidebarArea');
 			$f->addFieldToTab('Root.Blocks', $sidebarAreaField);
@@ -117,7 +111,7 @@ class DivisionPage extends DataExtension {
 
 		$beforecontentField = $f->dataFieldByName('BeforeContent');
 
-		if($beforecontentField){
+		if ($beforecontentField) {
 			$beforecontentField->setTitle('Before Content');
 			$f->remove($beforecontentField);
 			$f->addFieldToTab('Root.Blocks', $beforecontentField);
@@ -125,7 +119,7 @@ class DivisionPage extends DataExtension {
 
 		$beforecontentConstrainedField = $f->dataFieldByName('BeforeContentConstrained');
 
-		if($beforecontentConstrainedField){
+		if ($beforecontentConstrainedField) {
 			$beforecontentConstrainedField->setTitle('Before Content (Constrained)');
 			$f->removeByName('BeforeContentConstrained');
 			$f->addFieldToTab('Root.Blocks', $beforecontentConstrainedField);
@@ -133,7 +127,7 @@ class DivisionPage extends DataExtension {
 
 		$aftercontentAreaField = $f->dataFieldByName('AfterContent');
 
-		if($aftercontentAreaField){
+		if ($aftercontentAreaField) {
 			$aftercontentAreaField->setTitle('After Content');
 			$f->removeByName('AfterContent');
 			$f->addFieldToTab('Root.Blocks', $aftercontentAreaField);
@@ -141,7 +135,7 @@ class DivisionPage extends DataExtension {
 
 		$aftercontentConstrainedField = $f->dataFieldByName('AfterContentConstrained');
 
-		if($aftercontentConstrainedField){
+		if ($aftercontentConstrainedField) {
 			$aftercontentConstrainedField->setTitle('After Content (Constrained)');
 			$f->removeByName('AfterContentConstrained');
 			$f->addFieldToTab('Root.Blocks', $aftercontentConstrainedField);
@@ -150,137 +144,115 @@ class DivisionPage extends DataExtension {
 		$f->removeByName('ElementalArea');
 		$f->removeByName('ContentArea');
 
-
 		$config = SiteConfig::current_site_config();
 
-
-
 		if ($metadataField = $f->fieldByName('Root.Main.Metadata')) {
-			$f->removeFieldFromTab('Root.Main', 'Metadata');
-			$f->addFieldToTab('Root.MetaData', $metadataField);
+			$metadataField->setTitle('Summary');
+			$f->renameField('MetaDescription', 'Short summary of this content');
+			// 	$f->removeFieldFromTab('Root.Main', 'Metadata');
+			// 	$f->addFieldToTab('Root.MetaData', $metadataField);
 		}
 
 		if (Permission::check('ADMIN')) {
 			$f->addFieldToTab('Root.Main', new UploadField('BackgroundImage', 'Background Image (1600 x 500)'), 'Content');
 		}
-		$f->addFieldToTab('Root.SocialMediaSharing', new LiteralField('SocialMediaInfo','<p>All information placed in the fields below will override any fields filled out in the "Main Content" tab. <br /><em><a href="https://md.studentlife.uiowa.edu/clients/digital-marketing/sharing-content-on-facebook-best-practices/">Sharing content on Facebook: best practices &rarr;</a></em></p>'));
+		$f->addFieldToTab('Root.SocialMediaSharing', new LiteralField('SocialMediaInfo', '<p>All information placed in the fields below will override any fields filled out in the "Main Content" tab. <br /><em><a href="https://md.studentlife.uiowa.edu/clients/digital-marketing/sharing-content-on-facebook-best-practices/">Sharing content on Facebook: best practices &rarr;</a></em></p>'));
 
 		$f->addFieldToTab("Root.SocialMediaSharing", new UploadField('OgImage', 'Social Share Image'));
 		$f->addFieldToTab('Root.SocialMediaSharing', new TextField('OgTitle', 'Social Share Title'));
 		$f->addFieldToTab('Root.SocialMediaSharing', new TextareaField('OgDescription', 'Social Share Description'));
 
-		//I hate this. It needs to be moved to Root.Settings, but it breaks the homepage slider. Either this
-		//block or the settings area or both need to be refactored.
-
-		$layoutOptionsField = DropdownField::create(
-  			'LayoutType',
-  			'Layout type',
-  			$this->owner->LayoutTypes()
-		)->setEmptyString('(Default Layout)');
-		$f->addFieldToTab('Root.Main', $layoutOptionsField);
-
-		$f->addFieldsToTab("Root.Main", array(
-			$embed = \EdgarIndustries\YouTubeField\YouTubeField::create("YoutubeBackgroundEmbed","Video"
-		)), 'LayoutType');
-		$embed->displayIf('LayoutType')->isEqualTo('BackgroundVideo');
-		$f->addFieldsToTab("Root.Main", array(
-			$fullImgAlt = TextField::create("FullImageAltText","Alt Text For Background Image (required if image has text in it!)"
-		)->addExtraClass('stacked')), 'LayoutType');
-		$fullImgAlt->displayIf('LayoutType')->isEqualTo('FullImage');
-	// 	if($this->owner->getExistsOnLive() == true){
-	// 		//https://developers.facebook.com/tools/debug/sharing/?q=
-	// 		$f->addFieldToTab('Root.SocialMediaSharing', new LiteralField('FbShareButton','<a href="https://developers.facebook.com/tools/debug/sharing/?q='.$this->owner->AbsoluteLink().'" target="_blank" type="button" class="ss-ui-button ui-corner-all ui-button ui-widget ui-state-default ui-button-text-icon-primary" title="Share Page On Facebook" role="button" aria-disabled="false"><span class="ui-button-text">
-	// Preview Facebook Share</span></a>'));
-	// 		$f->addFieldToTab('Root.SocialMediaSharing', new LiteralField('FbShareButton','<a href="http://www.facebook.com/sharer/sharer.php?u='.$this->owner->AbsoluteLink().'" target="_blank" type="button" class="ss-button-fb ss-button-social ss-ui-button ui-corner-all ui-button ui-widget ui-state-default ui-button-text-icon-primary" title="Share Page On Facebook" role="button" aria-disabled="false"><span class="ui-button-text">
-	// Share Page On Facebook</span></a>'));
-	// 		$f->addFieldToTab('Root.SocialMediaSharing', new LiteralField('TwitterShareButton','<a href="https://twitter.com/intent/tweet?text='.$this->owner->AbsoluteLink().'" target="_blank" type="button" class="ss-button-twitter ss-button-social ss-ui-button-twitter ss-ui-button ui-corner-all ui-button ui-widget ui-state-default ui-button-text-icon-primary" title="Share Page On Twitter" role="button" aria-disabled="false"><span class="ui-button-text">
-	// 		Share Page On Twitter</span></a>'));
-	// 	}else{
-	// 		$f->addFieldToTab('Root.SocialMediaSharing', new LiteralField('ShareNotice','<p>The ability to share this page will appear after you\'ve published it.</p>'));
-	// 	}
-
-		$gridFieldConfig = GridFieldConfig_RelationEditor::create();
-		$parent = $this->owner->Parent();
-		if((isset($parent)) && ($parent->ClassName == 'FeatureHolderPage')){
-			$f->addFieldToTab('Root.Main', new UploadField('FeatureHolderImage', 'Feature Holder Image (shown in parent)'), 'Content');
-		}
-
-
 		$f->addFieldToTab('Root.Main', HTMLEditorField::create('Content')->addExtraClass('stacked'));
-
-
-
 
 	}
 
-	public function LayoutTypes(){
+	public function LayoutTypes() {
 		return $this->owner->stat('layout_types');
 	}
 
 	public function updateSettingsFields($f) {
 
-
-
 		$f->addFieldToTab('Root.Settings', CheckboxField::create('PreventSearchEngineIndex', 'Prevent search engines from indexing this page'));
-		$f->addFieldToTab('Root.Settings', CheckboxField::create('ShowChildPages','Show child pages if available (Yes)'));
-		$f->addFieldToTab('Root.Settings', CheckboxField::create('ShowChildrenInDropdown','Show child pages in a dropdown menu if page is in the top bar (Yes)'));
-		$f->addFieldToTab('Root.Settings', CheckboxField::create('DarkMode','Dark Mode (Experimental)'));
+		$f->addFieldToTab('Root.Settings', CheckboxField::create('ShowChildPages', 'Show child pages if available (Yes)'));
+		$f->addFieldToTab('Root.Settings', CheckboxField::create('ShowChildrenInDropdown', 'Show child pages in a dropdown menu if page is in the top bar (Yes)'));
+		$f->addFieldToTab('Root.Settings', CheckboxField::create('DarkMode', 'Dark Mode (Experimental)'));
+		//I hate this. It needs to be moved to Root.Settings, but it breaks the homepage slider. Either this
+		//block or the settings area or both need to be refactored.
 
+		$layoutOptionsField = DropdownField::create(
+			'LayoutType',
+			'Layout type',
+			$this->owner->LayoutTypes()
+		)->setEmptyString('(Default Layout)');
+		$f->addFieldToTab('Root.Settings', $layoutOptionsField);
+
+		$f->addFieldsToTab("Root.Settings", array(
+			$embed = \EdgarIndustries\YouTubeField\YouTubeField::create("YoutubeBackgroundEmbed", "Video"
+			)), 'LayoutType');
+		$embed->displayIf('LayoutType')->isEqualTo('BackgroundVideo');
+		$f->addFieldsToTab("Root.Settings", array(
+			$fullImgAlt = TextField::create("FullImageAltText", "Alt Text For Background Image (required if image has text in it!)"
+			)->addExtraClass('stacked')), 'LayoutType');
+		$fullImgAlt->displayIf('LayoutType')->isEqualTo('FullImage');
+
+		$gridFieldConfig = GridFieldConfig_RelationEditor::create();
+		$parent = $this->owner->Parent();
+		if ((isset($parent)) && ($parent->ClassName == 'FeatureHolderPage')) {
+			$f->addFieldToTab('Root.Settings', new UploadField('FeatureHolderImage', 'Feature Holder Image (shown in parent)'), 'Content');
+		}
 	}
 
 	public function getSidebarItems() {
 		return $this->owner->getManyManyComponents('SidebarItems')->sort('SortOrder');
 	}
 
-
-	public function getPageTypeTheme(){
-		if($this->owner->getPageTypeTheme){
+	public function getPageTypeTheme() {
+		if ($this->owner->getPageTypeTheme) {
 			// print_r($this->getPageTypeTheme);
 			return $this->owner->getPageTypeTheme;
 		}
 	}
 
-	public function DarkLightHeader(){
+	public function DarkLightHeader() {
 		$siteConfig = SiteConfig::current_site_config();
 		$owner = $this->owner;
 
 		//If the page type forces a particular dark/light scheme (eg homepage), defer to that first.
-		if($owner->pageTypeTheme){
+		if ($owner->pageTypeTheme) {
 			return $owner->pageTypeTheme;
-		//Check page's individual CMS setting:
-		}elseif($owner->DarkMode){
+			//Check page's individual CMS setting:
+		} elseif ($owner->DarkMode) {
 			return 'dark-header';
-		//Otherwise, check global settings
-		}elseif($siteConfig->UseDarkTheme){
+			//Otherwise, check global settings
+		} elseif ($siteConfig->UseDarkTheme) {
 			return 'dark-header';
 		}
 
 		return 'light-header';
 
-
 	}
 	//Frontend labels for various page types when the user sees them in site search results:
 	public function NiceName() {
 		$niceNames = array(
-			'Page'                => '',
-			'StaffPage'    		 => 'Staff Member',
-			'BlogPost'        	 => 'News',
-			'Blog'       		    => '',
-			'SiteConfigExtension' => ''
+			'Page' => '',
+			'StaffPage' => 'Staff Member',
+			'BlogPost' => 'News',
+			'Blog' => '',
+			'SiteConfigExtension' => '',
 		);
-		if(isset($niceNames[$this->owner->singular_name()])){
+		if (isset($niceNames[$this->owner->singular_name()])) {
 			$niceClassName = $niceNames[$this->owner->singular_name()];
 			return $niceClassName;
-		}else{
+		} else {
 			return preg_replace('/([a-z]+)([A-Z])/', '$1 $2', $this->owner->singular_name());
 		}
 	}
 
-	public function ContentSummary(){
+	public function ContentSummary() {
 
 		$str = $this->owner->Content;
 
-		if((empty($str)) || ($str == '')){
+		if ((empty($str)) || ($str == '')) {
 			return null;
 		}
 
@@ -291,75 +263,70 @@ class DivisionPage extends DataExtension {
 
 		$res = $xp->query('//p');
 
-		if($res[0]){
+		if ($res[0]) {
 			$firstParagraph = $res[0]->nodeValue;
 			return $firstParagraph;
 		}
 
-
 	}
-
 
 //Shortcodes:
 
-	public static function StaffHolderShortcode($arguments, $content = null, $parser = null, $tagname){
+	public static function StaffHolderShortcode($arguments, $content = null, $parser = null, $tagname) {
 		$template = new SSViewer('StaffHolderShortcode');
 
-		$holder = StaffHolderPage::get()->filter(array('ID'=>$arguments['id']))->First();
+		$holder = StaffHolderPage::get()->filter(array('ID' => $arguments['id']))->First();
 
-		if(!$holder){
+		if (!$holder) {
 			return null;
 		}
 
 		$customise = array(
-			'StaffHolder' => $holder
+			'StaffHolder' => $holder,
 		);
 
 		return $template->process(new ArrayData($customise));
 	}
 
-
-	public static function ExpandShortcode($arguments, $content = null, $parser = null, $tagName){
+	public static function ExpandShortcode($arguments, $content = null, $parser = null, $tagName) {
 
 		$template = new SSViewer('ExpandShortcode');
 		$title = false;
 		$image = false;
 
-
 		//Get expander content from another page:
-		if(isset($arguments['page'])){
-			if(is_numeric($arguments['page']))
-			$page = SiteTree::get()->filter(array('ID' => $arguments['page']))->First();
+		if (isset($arguments['page'])) {
+			if (is_numeric($arguments['page'])) {
+				$page = SiteTree::get()->filter(array('ID' => $arguments['page']))->First();
+			}
 
-			if($page){
+			if ($page) {
 				//Reparse shortcodes when getting content from another page since we're acting in a shortcode itself right now.
 				$parser = ShortcodeParser::get();
 				$parsedContent = $parser->parse($page->Content);
 				$content = $parsedContent;
 			}
 		}
-		
-		if(isset($arguments['title'])){
+
+		if (isset($arguments['title'])) {
 			$title = $arguments['title'];
-		}elseif(isset($page)){
+		} elseif (isset($page)) {
 			$title = $page->Title;
 		}
 
-		if(isset($arguments['image'])){
+		if (isset($arguments['image'])) {
 			$image = $arguments['image'];
 		}
 
 		$customise = array(
 			'Title' => $title,
 			'ImageURL' => $image,
-			'Content' => $content
+			'Content' => $content,
 		);
-
 
 		return $template->process(new ArrayData($customise));
 
 		//return "<em>" . $tagName . "</em> " . $content . "; " . count($arguments) . " arguments.";
 	}
-
 
 }
